f286861d5302d20b6f6e19dd2acbffc8d547a884
Alexei Scerbakov <scerbakov.alexei@gmail.com>
2017-01-07 17:26:29 +0100

    Add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,43 @@
+const{ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '586e8a0321c0bc2826bad7c111';
+var userId = '586e8cd8fece00840c050b36';
+
+//if(!ObjectID.isValid(id)) {
+//    console.log('ID not valid');
+//}
+
+//Todo.find({
+//    _id: id
+//}).then((todos) => {
+//    console.log('Todos', todos);
+//});
+//
+//Todo.findOne({
+//    _id: id
+//}).then((todo) => {
+//    console.log('Todo', todo);
+//});
+//
+//Todo.findById(id).then((todo) => {
+//    if (!todo) {
+//        return console.log('Id not found');
+//    }
+//    console.log('Todo By Id', todo);
+//}).catch((e) => {
+//    console.log(e);
+//});
+
+User.findById(userId).then((user) => {
+    if (!user) {
+        return console.log('User Id not found');
+    }
+    console.log('User By Id', user);
+}).catch((e) => {
+    console.log(e);
+});
+
